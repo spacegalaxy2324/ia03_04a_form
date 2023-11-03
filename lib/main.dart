8df93248be2f25fb0cb17eb4ca43e024bf529700
spacegalaxy2324 <148562155+spacegalaxy2324@users.noreply.github.com>
2023-11-03 15:58:20 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'M08 - Form (A)',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+  final String title = 'Adrián López Villalba';
+  final _formKey = GlobalKey<FormBuilderState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const FormTitle(),
+            FormBuilder(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //-------------------------------------------------
+                  FormLabelGroup(
+                    title: 'Please provide the speed of vehicle?',
+                    subtitle: 'please select one option given below',
+                  ),
+                  FormBuilderRadioGroup(
+                    decoration: const InputDecoration(border: InputBorder.none),
+                    name: "speed",
+                    orientation: OptionsOrientation.vertical,
+                    // separator: const Padding(padding: EdgeInsets.all(20)),
+                    options: const [
+                      FormBuilderFieldOption(value: 'abvoe 40km/h'),
+                      FormBuilderFieldOption(value: 'below 40km/h'),
+                      FormBuilderFieldOption(value: '0km/h')
+                    ],
+                  ),
+                  //-------------------------------------------------
+                  FormLabelGroup(title: 'Enter remarks'),
+                  FormBuilderTextField(
+                    name: 'remark',
+                    decoration: InputDecoration(
+                      hintText: 'Enter your remarks',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      filled: true,
+                    ),
+                  ),
+                  //-------------------------------------------------
+                  FormLabelGroup(
+                    title: 'Please provide the high speed of vehicle?',
+                    subtitle: 'please select one option given below',
+                  ),
+                  FormBuilderDropdown(
+                    name: 'highspeed',
+                    decoration: InputDecoration(
+                      hintText: 'Select option',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    items: const [
+                      DropdownMenuItem(
+                          value: 'example', child: Text('Example')),
+                    ],
+                  ),
+                  //-------------------------------------------------
+                  FormLabelGroup(
+                    title: 'Please provide the speed of vehicle past 1 hour?',
+                    subtitle: 'please select one or more options given below',
+                  ),
+                  FormBuilderCheckboxGroup(
+                    decoration: const InputDecoration(border: InputBorder.none),
+                    name: "selectSpeed",
+                    orientation: OptionsOrientation.vertical,
+                    // separator: const Padding(padding: EdgeInsets.all(20)),
+                    options: const [
+                      FormBuilderFieldOption(value: '20km/h'),
+                      FormBuilderFieldOption(value: '30km/h'),
+                      FormBuilderFieldOption(value: '40km/h'),
+                      FormBuilderFieldOption(value: '50km/h'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FormLabelGroup extends StatelessWidget {
+  FormLabelGroup({super.key, required this.title, this.subtitle});
+
+  String title;
+  String? subtitle;
+
+  Widget conditionalSubtitle(BuildContext context) {
+    if (subtitle != null) {
+      return Text(subtitle!,
+          style: Theme.of(context).textTheme.labelLarge?.apply(
+              fontSizeFactor: 1.25,
+              color: Theme.of(context).colorScheme.secondary.withOpacity(0.5)));
+    } else {
+      return Container();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 20.0, 0, 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title,
+              style: Theme.of(context)
+                  .textTheme
+                  .labelLarge
+                  ?.apply(fontSizeFactor: 1.25)),
+          conditionalSubtitle(context)
+        ],
+      ),
+    );
+  }
+}
+
+class FormTitle extends StatelessWidget {
+  const FormTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            'Form title',
+            style: Theme.of(context)
+                .textTheme
+                .displaySmall
+                ?.copyWith(fontWeight: FontWeight.bold),
+          ),
+          Text('description', style: Theme.of(context).textTheme.labelLarge),
+        ],
+      ),
+    );
+  }
+}
